53a1d39252f45868602cd3e4b922799b043b2912
Diana Manarim Dias <dianamdias03@gmail.com>
2024-02-20 20:42:47 -0300

    Added whatsapp link

--- src/components/contact-button/index.js
+++ src/components/contact-button/index.js
@@ -5,7 +5,9 @@ export const ContactButton = ({color}) => {
     const className = `info-button ${color}-color`
     return (
         <>
-            <div className={className}><WhatsAppOutlined style={{ fontSize: '15px', marginBottom: '-3px' }}/>ENTRE EM CONTATO</div>
+            <button onClick={() => {
+               window.open('https://wa.me/5548991555418', '_blank', 'noopener,noreferrer') 
+            }} className={className}><WhatsAppOutlined style={{ fontSize: '15px', marginBottom: '-3px' }}/>ENTRE EM CONTATO</button>
         </>
       );
   };
--- src/sections/Header/index.js
+++ src/sections/Header/index.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './style.css'
 import {WhatsAppOutlined} from '@ant-design/icons'
-import { ContactButton } from '../../components/contact-button';
+//import { ContactButton } from '../../components/contact-button';
 
 export const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  /*const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  };
+  };*/
 
   return (
     <div className="border">
@@ -17,16 +17,18 @@ export const Header = () => {
         <img src="/logo.webp" alt="Logo" />
         <span style={{color: '#4B9AD8', fontWeight: 'bold'}}>OXYN</span>
       </div>
-      <nav className={`menu ${isMenuOpen ? 'is-open' : ''}`}>
+      {/*<nav className={`menu ${isMenuOpen ? 'is-open' : ''}`}>
         <ul>
           <li><a href="#home">Lorem</a></li>
           <li><a href="#about">Ipsum</a></li>
           <li><a href="#services">Dolor</a></li>
           <li><a href="#contact">Sit Amet</a></li>
         </ul>
-      </nav>
-      <div className="contact-info"><WhatsAppOutlined style={{ fontSize: '15px', marginBottom: '-3px' }}/> (48) 00000-0000</div>
-      <button className="menu-toggle" onClick={toggleMenu}>☰</button>
+  </nav>*/}
+      <button onClick={() => {
+               window.open('https://wa.me/5548991555418', '_blank', 'noopener,noreferrer') 
+            }} className="contact-info"><WhatsAppOutlined style={{ fontSize: '15px', marginBottom: '-1px' }}/> (48) 99155-5418</button>
+      {/*<button className="menu-toggle" onClick={toggleMenu}>☰</button>*/}
     </header>
     </div>
   );
