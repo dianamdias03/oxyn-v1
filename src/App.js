057737aa53f85d3d4f25e048d51067bf79a2300c
Diana Manarim Dias <dianamdias03@gmail.com>
2024-02-20 09:12:33 -0300

    Adjusted paragraphs

--- src/App.js
+++ src/App.js
@@ -9,40 +9,40 @@ function App() {
     <div className="App">
       <Header/>
       <TextImage 
-        subtitle={'AGÊNCIA DE MARKETING'} 
-        title={'São muitos os desafios dos primeiros passos na carreira de psicologia clínica'}
-        paragraphs={['Imagine dedicar-se totalmente aos seus pacientes, enquanto a Oxyn Pages conecta você a pessoas que precisam de atendimento. Você pode dedicar-se em se aprimorar no atendimento clinico e nós cuidamos do resto.']} 
-        paragraphBold={'Clique no botão abaixo e descubra como podemos te ajudar.'}
+        subtitle={'MARKETING PARA PSICOLOGO RECEM FORMADO'} 
+        title={'Transforme Sua Formação em Sucesso: Marketing para Psicólogos Clínicos que Estão Começando!'}
+        paragraphs={['Entendemos os desafios de dar os primeiros passos na sua carreira clínica e estamos aqui para simplificar sua jornada. Com estratégias de marketing digital feitas sob medida, você pode focar no que realmente importa: seus pacientes.', 
+        'Você não conhecer marketing não é problema. Nós cuidamos de tudo para que você apareça na primeira página do Google quando alguém buscar por atendimento clínico em psicologia.',
+        'Tenha um site que reflita sua profissionalidade, com fotos e especialidades que destacam seus serviços, e se torne a escolha preferida de quem busca ajuda.']} 
+        paragraphBold={'Transforme Dúvidas em Ação: Entre em contato agora mesmo!'}
         image={'/about.webp'}
         altImage={'Homem olhando para o celular com notificações'}
         imageRightSide={true}
       />
       <TextImage 
-        subtitle={'NÃO DEIXE DINHEIRO NA MESA'} 
-        title={'Sabemos que o inicio da prática clinica traz vários desafios e dentre eles, a está a questão financeira e a dificuldade em conseguir novos clientes.'}
+        subtitle={'TRANSFORME SUA PRESENÇA DIGITAL E ATRAIA MAIS PACIENTES!'} 
+        title={'Conquiste Seu Espaço: Marketing na Internet Ajustado para o Seu Momento Profissional!'}
         paragraphs={[
-          'Pensando nisso a Oxyn Pages trabalha com planos de pagamento facilitado para quem está iniciando. O dinheiro investido retorna dentro do mesmo mês, sem comprometer suas finanças pessoais.'
-          ,'Com os clientes que vamos ajudar você a conseguir, sua prática clinica não apenas sobreviverá, mas prosperará. Você terá a liberdade de focar no seu desenvolvimento profissional, expandir suas habilidades e, o mais importante, transformar a vida de seus pacientes.'
+          'Imagine abrir seu WhatsApp todos os dias com novas mensagens de potenciais pacientes. Esse é o poder do marketing digital acessível e eficaz. Clique agora e dê o primeiro passo para transformar sua formação em um sucesso palpável.',
+          'Elimine as barreiras geográficas e amplie seu alcance. Com nosso suporte, você pode oferecer atendimento online e conectar-se com pacientes de todas as regiões do país, maximizando sua visibilidade e oportunidades de crescimento.'
         ]}
-        paragraphBold={'Está pronta para deixar as incertezas para trás e dar um grande passo em direção ao sucesso na sua carreira? Clique no botão abaixo e vamos juntos construir o futuro próspero que você e sua prática clínica merecem.'}
+        paragraphBold={'Deixe as incertezas de lado e embarque em uma jornada rumo ao sucesso. Junte-se a nós e construa o futuro brilhante que você merece.'}
         image={'/finances.webp'}
         altImage={'Planta simbolizando economia'}
         imageRightSide={true}
       />
       <Text title={'Juntos, vamos transformar desafios em oportunidades.'} paragraphs={['Descubra como a Oxyn Pages pode ajudar você a se conectar com aqueles que estão procurando exatamente o que você tem a oferecer.']} background={'blue'}/>
       <TextImage 
-        title={'Sabemos que cada pessoa tem suas próprias necessidades e preocupações'}
+        title={'Como funciona:'}
         paragraphs={[
-          'Especialmente quando se trata de finanças e da construção de sua clientela. Queremos te ajudar com isso.'
-          ,'Com os clientes que vamos ajudar você a conseguir, sua prática clinica não apenas sobreviverá, mas prosperará. Você terá a liberdade de focar no seu desenvolvimento profissional, expandir suas habilidades e, o mais importante, transformar a vida de seus pacientes.'
-          ,'Sabemos que cada pessoa tem suas próprias necessidades e preocupações, especialmente quando se trata de finanças e da construção de sua clientela. Queremos te ajudar com isso.'
+          'Ao clicar no botão "ENTRE EM CONTATO", você pode sanar qualquer dúvida via Whataspp, usado texto ou audio ou ainda combinar uma conversa de tira-dúvidas.',
+          'Importante: Você só paga após seu site e estrutura de marketing estiver funcionando. Caso não goste do serviço, você pode cancelar dentro de 30 e devolveremos o seu dinheiro.'
         ]}
         paragraphBold={'Clique no botão abaixo, vamos nos conhecer e resolver todas as duvidas'}
         image={'/therapy.webp'}
         altImage={'Planta simbolizando economia'}
         imageRightSide={false}
       />
-      <Text title={'Você só paga após seu site e estrutura de marketing estiver funcionando'} paragraphs={['Caso não goste do serviço, você pode cancelar dentro de 30 e devolveremos o seu dinheiro.']} background={'grey'}/>
       <Footer/>
     </div>
   );
